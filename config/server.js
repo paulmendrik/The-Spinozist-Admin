1f2d4ae64b6525a8414877a92880ed82654d25e7
Paul Mendrik <paul@imouse.net>
2021-06-16 13:50:17 +0200

    Commit All Files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0769f567340f7b8a960308d4bb037f44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72c1dc90-b2b9-4836-a342-e2c8c9673b96'
+};
